c8cc221fd465fe43d6e1e88570d1efedaaaad311
Christoph Rohmann <c.rohmann@salt-and-pepper.eu>
2018-02-28 13:30:07 +0100

    duplicated project configuration to x64 target

--- format/cpp/LightFieldWavelet/LightFieldVaveletTests/main.cpp
+++ format/cpp/LightFieldWavelet/LightFieldVaveletTests/main.cpp
@@ -6,6 +6,7 @@ using namespace SubdivisionShpere;
 int main()
 {
 	std::cout << "light field wavelet format cpp start" << std::endl;	
-	SubdivisionSphere* subsphere = new SubdivisionSphere(7);
+	SubdivisionSphere* subsphere = new SubdivisionSphere(12);
+	delete subsphere;
 	return 0;
 }
